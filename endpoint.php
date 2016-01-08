b3812c26aa9227cb7326848e2fa71b99de38db45
antonioortegajr <antonioortegajr@gmail.com>
2016-01-08 08:34:34 -0800

    is json and added more checks
    added checks based on enhancement issues and made the output json

--- endpoint.php
+++ endpoint.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Sys Profile
 Plugin URI: http://antonioortegajr.com
 Description: This plugin creates a custom REST API Endpoint that displays information on the WordPress install and the stack it runs on.
-Version: 0.1
+Version: 0.5
 Author: Antonio Ortega Jr
 Author URI: http://antonioortegajr.com
 License: GPL2
@@ -13,14 +13,32 @@ This registers the plugin. Functionality is currently under construction. Based 
 */
 
 add_action( 'rest_api_init', function () {
-    register_rest_route( 'myplugin/v1', '/author', array(
+    register_rest_route( 'sys_profile/v1', '/profile', array(
         'methods' => 'GET',
-        'callback' => 'my_awesome_func',
+        'callback' => 'sys_profile_func',
     ) );
 } );
-function my_awesome_func( WP_REST_Request $request ){
+function sys_profile_func( WP_REST_Request $request ){
 
-$output = get_site_url();
+  // Check if get_plugins() function exists. This is required on the front end of the
+  // site, since it is in a file that is normally only loaded in the admin.
+  if ( ! function_exists( 'get_plugins' ) ) {
+  	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+  }
+
+  $all_plugins = get_plugins();
+
+  //profile this WordPress account
+      $output = array(
+      'url'=>get_site_url(),
+      'php_version'=>phpversion(),
+      'wordpress_Version'=>get_bloginfo('version'),
+      'encoding'=>get_bloginfo('charset'),
+      'multi_site'=>is_multisite(),
+      'current_theme'=>get_current_theme(),
+      'installed_plugins'=>$all_plugins
+      );
+
 
 return $output;
 
